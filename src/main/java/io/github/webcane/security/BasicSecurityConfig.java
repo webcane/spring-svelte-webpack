39b0e7795cc2a04637e0813ee317f7202df26ada
Mikhail Niedre <mikhail.niedre@t-systems.com>
2020-11-05 01:04:54 +0300

    redirect back after login and logout

--- src/main/java/io/github/webcane/security/BasicSecurityConfig.java
+++ src/main/java/io/github/webcane/security/BasicSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -45,11 +46,12 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginProcessingUrl("/login")
-                // .successForwardUrl("http://localhost:3000")
+                .successHandler(new SimpleUrlAuthenticationSuccessHandler("http://localhost:3000"))
                 .permitAll()
                 .and()
                 .logout()
                 .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("http://localhost:3000")
                 .permitAll()
                 .and()
                 .csrf()
